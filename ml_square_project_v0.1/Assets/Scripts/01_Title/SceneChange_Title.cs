78c9d01a03b0e73889ba21bd3fef72fe05c053c5
DaichiYoshizaki <daichi.yoshizaki@monstar-lab.com>
2016-10-18 11:16:56 +0900

    Revert "Revert "logo, title, fade""

---
+++ ml_square_project_v0.1/Assets/Scripts/01_Title/SceneChange_Title.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange_Title : MonoBehaviour {
+	GameObject fade;
+
+	// Use this for initialization
+	void Start () {
+		fade = GameObject.Find("Fade");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0 || Input.GetMouseButtonDown(0))
+		{
+			fade.SendMessage("FadeOut", "02_SelectStage");
+		}
+	}
+}
