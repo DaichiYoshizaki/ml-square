7e087b1cbe28a8f13a13ba8173e09e70ac2766eb
DaichiYoshizaki <daichiyoshizaki@Administrator-no-MacBook-Pro.local>
2016-10-19 16:57:01 +0900

    201610191656

--- ml_square_project_v0.1/Assets/Scripts/03_Game/jumpButton/jumpButton.cs
+++ ml_square_project_v0.1/Assets/Scripts/03_Game/jumpButton/jumpButton.cs
@@ -8,20 +8,24 @@ public class jumpButton : MonoBehaviour {
 	private playerMover playerMover;
 
 	public void PushDown(){
-		push = true;
-		playerMover.IsAbleToMove = false;
+		if (playerMover.IsAbleToJump) {
+			push = true;
+			playerMover.IsAbleToMove = false;
+		}
 	}
 
 	public void PushUp(){
-		push = false;
-		if (jumpPower > 10f) {
-			playerMover.jump (1000f);
-		} else {			
-			playerMover.jump (500f);
+		if (playerMover.IsAbleToJump && push) {
+			push = false;
+			if (jumpPower > 10f) {
+				playerMover.jump (2500f);
+			} else {			
+				playerMover.jump (1700f);
+			}
+			jumpPower = 0f;
+			playerMover.IsAbleToMove = true;
+			playerMover.IsAbleToJump = false;
 		}
-		jumpPower = 0f;
-		playerMover.IsAbleToMove = true;
-		playerMover.IsAbleToJump = false;
 	}
 	void Start(){
 		playerMover = player.GetComponent<playerMover> ();
