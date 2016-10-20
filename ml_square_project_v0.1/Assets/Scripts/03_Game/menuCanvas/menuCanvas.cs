07e53bb5575b1a5df15417853640976fd195429c
DaichiYoshizaki <daichiyoshizaki@Administrator-no-MacBook-Pro.local>
2016-10-20 11:08:18 +0900

    add pauseSystem

---
+++ ml_square_project_v0.1/Assets/Scripts/03_Game/menuCanvas/menuCanvas.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuCanvas : MonoBehaviour {
+
+    static private bool opened = false;
+
+    static public void Open(){
+        
+    }
+
+    static public void Close() { 
+    }
+
+    void Awake(){
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+}
---
+++ ml_square_project_v0.1/Assets/Scripts/03_Game/pauseButton/pauseBottan.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseBottan : MonoBehaviour{
+    public void OnClick(){
+        pauser.Pause();
+    }
+}
