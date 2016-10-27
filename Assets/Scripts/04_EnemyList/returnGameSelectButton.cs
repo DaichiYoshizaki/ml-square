3a7af39b18930d38a37f0e592ece66bf59f05833
DaichiYoshizaki <daichiyoshizaki@Administrator-no-MacBook-Pro.local>
2016-10-27 21:02:14 +0900

    ketugou all

--- Assets/Scripts/04_EnemyList/returnGameSelectButton.cs
+++ Assets/Scripts/04_EnemyList/returnGameSelectButton.cs
@@ -4,6 +4,6 @@ using System.Collections;
 public class returnGameSelectButton : MonoBehaviour {
 	public void OnClick(){
 		SoundManager.Instance.PlaySE (0);
-		Fade.instance.FadeOut ("03_Game", 3f, 3f);
+		Fade.instance.FadeOut ("02_SelectStage", 3f, 3f);
 	}
 }
