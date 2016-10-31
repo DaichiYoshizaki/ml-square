9444250c9d85e87f22e791788bea91f85aae6115
DaichiYoshizaki <daichiyoshizaki@Administrator-no-MacBook-Pro.local>
2016-10-31 16:12:26 +0900

    originalStage

---
+++ Assets/Scripts/03_Game/gameManager/gameManagerOriginal.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class gameManagerOriginal : MonoBehaviour {
+	private GameObject player;
+	private GameObject gameCamera;
+	private List<GameObject> stages = new List<GameObject>{null, null, null};
+	private List<GameObject> playerSpawn = new List<GameObject>{null, null, null};
+	static public int currentStageIndex = 0;
+	public bool isDebugMode = false;
+	public int stage = 0;
+	public int AreaSelectIndex = 0;
+	static public int areaSelectIndex;
+	static public bool isStageRespawn = false;
+
+	void Awake(){
+	}
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		stages [0] = GameObject.Find ("PauseObjects/area/gameStage1");
+		stages [1] = GameObject.Find ("PauseObjects/area/gameStage2");
+		stages [2] = GameObject.Find ("PauseObjects/area/gameStage3");
+
+		playerSpawn [0] = GameObject.Find ("gameStage1/spawnPoint");
+		playerSpawn [1] = GameObject.Find ("gameStage2/spawnPoint");
+		playerSpawn [2] = GameObject.Find ("gameStage3/spawnPoint");
+
+		player = GameObject.Find ("PauseObjects/gamePlayer");
+
+		gameCamera = GameObject.Find ("gameCamera");
+
+		player.transform.position = playerSpawn [currentStageIndex].transform.position;
+
+
+		SoundManager.Instance.PlayBGM (1);
+		if (ManagerSelectStage.TheCurrentlySelectStageID == 7) {
+			SoundManager.Instance.StopBGM ();
+			SoundManager.Instance.PlayBGM (2);
+		}
+
+		if (isDebugMode) {
+			player.transform.position = playerSpawn [areaSelectIndex].transform.position;
+			gameCamera.transform.position = new Vector3 (stages [areaSelectIndex].transform.position.x, gameCamera.transform.position.y, gameCamera.transform.position.z);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+}
---
+++ Assets/Scripts/Editor/stageAllEdit.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+public class stageAllCreate{
+	[MenuItem("StageEdit/allCreate")]
+	static public void allCreate(){
+		string[] guids = new string[99];
+		string path = AssetDatabase.GUIDToAssetPath (guids [0]);
+		stageData obj = AssetDatabase.LoadAssetAtPath<stageData> (path);
+
+		for(int allCnt = 0; allCnt < 99; allCnt++){
+				stageData sd = new stageData{ };
+			if (Resources.Load ("OriginalStageData/stage" + allCnt.ToString ())) {
+					string a = "Resources/OriginalStageData/stage" + allCnt.ToString ();
+					AssetDatabase.DeleteAsset (a);
+				}
+				saveStageData.CreateAsset (sd, allCnt);
+				AssetDatabase.Refresh ();
+		}
+
+	}
+
+}
